8f5c1ce65ebcbef187ce6c4fed4b47cbcbb8d006
Ori Suchy <orisuchy@gmail.com>
2021-03-21 12:31:46 +0200

    Init project
    Copy from 2D UFO

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public int numOfPickups;
+    public Text countText;
+    public Text dizzyTimer;
+    public Text speedBoostTimer;
+    public Text winText;
+    public float speed;
+    private Rigidbody2D rb2d;
+    private int score;
+    private float timeBoost=10;
+    private bool activeBoost=false;
+    private float timeDizzy=10;
+    private bool activeDizzy=false;
+    
+    void Start(){
+        rb2d=GetComponent<Rigidbody2D>();
+        score=0;
+        winText.text="";
+        dizzyTimer.text = "";
+        speedBoostTimer.text = "";
+        SetCountText();
+    }
+    void FixedUpdate(){
+        
+        float moveHorizontal=Input.GetAxis("Horizontal");
+        float moveVertical=Input.GetAxis("Vertical");
+        Vector2 movement=new Vector2(moveHorizontal,moveVertical);
+        if(activeBoost){
+            if(timeBoost>0){
+                timeBoost-= Time.deltaTime;
+                speedBoostTimer.text = "End of speed boost: " + System.Math.Round(timeBoost).ToString();
+            }
+            else{
+                speed /= 7;
+                activeBoost =false;
+                timeBoost=10;
+                speedBoostTimer.text = "";
+            }
+            
+        }
+        if(activeDizzy){
+            if(timeDizzy>0){
+                timeDizzy-= Time.deltaTime;
+                dizzyTimer.text = "End of dizzy: " + System.Math.Round(timeDizzy).ToString();
+            }
+            else{
+                speed = -speed;
+                activeDizzy =false;
+                timeDizzy=10;
+                dizzyTimer.text = "";
+            }
+            
+        }
+        rb2d.AddForce(movement*speed);
+        if(Input.GetKey("escape")){
+            Application.Quit();
+        }
+
+    }
+    
+    void OnTriggerEnter2D(Collider2D other){
+        if (other.gameObject.CompareTag("PickUp")){
+            other.gameObject.SetActive(false);
+            score++;
+            numOfPickups--;
+            SetCountText();
+        }
+        else if(other.gameObject.CompareTag("SpeedBoost")){
+            other.gameObject.SetActive(false);
+            speed=speed*7;
+            activeBoost=true;
+            
+        }
+        else if(other.gameObject.CompareTag("Saw")){            
+            score=score-2;
+            SetCountText();
+            if(!activeDizzy){
+                speed=-speed;
+                activeDizzy=true;
+            }
+            
+        }
+
+    }
+    void SetCountText(){
+        countText.text="Score: "+score.ToString();
+        if(numOfPickups == 0)
+        {
+            winText.text="You won!!\nYour score is " + score.ToString();
+        }
+    }    
+}
---
+++ Assets/Scripts/randomMovement.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomMovement : MonoBehaviour
+{
+    public enum movementOptions {JUMP, GLIDE}
+    public movementOptions moveOption;
+    private float randomWaitingTime;
+    private float objWidth;
+    private float objheight;
+    private Vector3 nextPos;
+    private float speed = 6;
+    private bool isMoving = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rb2d = GetComponent<Rigidbody2D>();
+        randomWaitingTime = Random.Range(1, 10);
+        objWidth = getObjWidth();
+        objheight = getObjHeight();
+}
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (isMoving)
+        {
+            gameObject.transform.position = Vector3.MoveTowards(transform.position, nextPos, Time.deltaTime * speed);
+            if (gameObject.transform.position == nextPos) { isMoving = false; }
+        }
+        if (randomWaitingTime > 0 && !isMoving)
+        {
+            randomWaitingTime-= Time.deltaTime;
+        }
+        else if (randomWaitingTime <= 0 && !isMoving)
+        {
+            float nextX = Random.Range(-12.5f + objWidth/2, 12.5f - objWidth/2);
+            float nextY = Random.Range(-12.5f + objheight/2, 12.5f - objheight/2);
+            nextPos = new Vector3(nextX, nextY, 0);
+
+            if (moveOption == 0)
+            {
+                gameObject.transform.position = nextPos;
+            }
+            else
+            {
+                //gameObject.transform.position = Vector3.MoveTowards(transform.position, nextPos, Time.deltaTime * speed);
+                //gameObject.transform.position = Vector3.Lerp(transform.position, nextPos, 0.1f);
+                isMoving = true;
+            }
+            randomWaitingTime = Random.Range(1, 10);
+        }
+        
+
+        
+    }
+
+    private float getObjHeight()
+    {
+        var p1 = gameObject.transform.TransformPoint(0, 0, 0);
+        var p2 = gameObject.transform.TransformPoint(1, 1, 0);
+        return (p2.x - p1.x);
+        
+    }
+
+    private float getObjWidth()
+    {
+        var p1 = gameObject.transform.TransformPoint(0, 0, 0);
+        var p2 = gameObject.transform.TransformPoint(1, 1, 0);
+        return (p2.y - p1.y);
+    }
+
+}
